2aac0ddb51b8224c16a8a51ec57e2c29ea1ceb31
Michel Figueira <michelfigueiratemp@gmail.com>
2022-09-30 21:11:49 -0300

    NGRX First Action

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { map } from 'rxjs';
+
+import { decrementaContador, IAppState, incrementaContador } from './store/app.state';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +11,21 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'NgRxTests';
+
+  constructor(private store: Store<{app: IAppState}>) {
+  }
+
+  counter$ = this.store
+    .select('app')
+    .pipe(
+      map(e => e.counter)
+  );
+
+  incrementaContador() {
+    this.store.dispatch(incrementaContador())
+  }
+
+  decrementaContador() {
+    this.store.dispatch(decrementaContador())
+  }
 }
